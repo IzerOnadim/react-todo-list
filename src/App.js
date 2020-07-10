98e7d7527aec25f59854ae379035dec66c07ab4e
IzerOnadim <izer.onadim19@imperial.ac.uk>
2020-07-10 17:18:07 +0100

    Add a list of bullet points

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>To-Do List</h1>
+      <ul>
+        <li>This is an example list</li>
+        <li>Here is another bullet point</li>
+      </ul>
     </div>
   );
 }
