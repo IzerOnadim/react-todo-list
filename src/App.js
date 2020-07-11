cb02e2b7d4cf87e57a61ce1459ba38aa309e57ed
IzerOnadim <izer.onadim19@imperial.ac.uk>
2020-07-11 23:04:32 +0100

    Switch function component to class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>To-Do List</h1>
-      <ul>
-        <li>This is an example list</li>
-        <li>Here is another bullet point</li>
-      </ul>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
 }
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <h1>To-Do List</h1>
+//      <ul>
+//        <li>This is an example list</li>
+//        <li>Here is another bullet point</li>
+//      </ul>
+//    </div>
+//  );
+//}
 
 export default App;
